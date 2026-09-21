9bf2bd7c997dfea0ac3a8d3b7a1e6903ee6a2c7f
BranN01 <xpetkob00@vutbr.cz>
2026-09-21 20:04:59 +0200

    Implemented half of the Strassen  algorithm

--- src/strassen.c
+++ src/strassen.c
@@ -17,7 +17,7 @@ void strassen(unsigned n) {
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
 
-    // ALG
+    compute_strassen(g.mtx_A, g.mtx_B, n);
 
     clock_gettime(CLOCK_MONOTONIC, &end);
     double sec = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
@@ -28,4 +28,66 @@ void strassen(unsigned n) {
 
     // clearing C matrix
     memset(g.mtx_C, 0, n * n * sizeof(float));
+}
+
+void compute_strassen(float *A, unsigned stride_A, float *B, unsigned stride_B,
+                      float *C, unsigned stride_C, unsigned n) {
+
+    // 
+    if(n <= BASIC_START) {
+        for(unsigned idx = 0; idx < n; idx++) {
+            for(unsigned kdx = 0; kdx < n; kdx++) {
+                float reg = A[idx * stride_A + kdx];
+                for(unsigned jdx = 0; jdx < n; jdx++) {
+                    C[idx * stride_C + jdx] += reg * B[kdx * stride_B + jdx];
+                }
+            }
+        }
+        return;
+    }
+
+
+    unsigned half = n / 2;
+    unsigned long block = half * half;
+
+    float *A11 = A, *A12 = A + half, *A21 = A + half * stride_A, *A22 = A + half * stride_A + half;
+    float *B11 = B, *B12 = B + half, *B21 = B + half * stride_B, *B22 = B + half * stride_B + half;
+    
+
+    // multiplication TODO
+
+
+    
+    float *C11 = C, *C12 = C + half, *C21 = C + half * stride_C, *C22 = C + half * stride_C + half;
+
+    for(unsigned idx = 0; idx < half; idx++) {
+        for(unsigned jdx = 0; jdx < half; jdx++) {
+            unsigned pos = idx * half + jdx;
+
+            C11[idx * stride_C + jdx] += I[pos] + IV[pos] - V[pos] + VII[pos];
+            C12[idx * stride_C + jdx] += III[pos] + V[pos];
+            C21[idx * stride_C + jdx] += II[pos] + IV[pos];
+            C22[idx * stride_C + jdx] += I[pos] + III[pos] - II[pos] + VI[pos];
+        }
+    }
+}
+
+void add(float *X, unsigned stride_X, float *Y, unsigned stride_Y,
+         float *Z, unsigned stride_Z, unsigned n) {
+    
+    for(unsigned idx = 0; idx < n; idx++) {
+        for(unsigned jdx = 0; jdx < n; jdx++) {
+            Z[idx * stride_Z + jdx] = X[idx * stride_X + jdx] + Y[idx * stride_Y + jdx];
+        }
+    }
+}
+
+void sub(float *X, unsigned stride_X, float *Y, unsigned stride_Y,
+         float *Z, unsigned stride_Z, unsigned n) {
+    
+    for(unsigned idx = 0; idx < n; idx++) {
+        for(unsigned jdx = 0; jdx < n; jdx++) {
+            Z[idx * stride_Z + jdx] = X[idx * stride_X + jdx] - Y[idx * stride_Y + jdx];
+        }
+    }
 }
--- src/strassen.h
+++ src/strassen.h
@@ -11,6 +11,7 @@
 #define STRASSEN_H
 
 #define _POSIX_C_SOURCE 199309L
+#define BASIC_START 16
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,5 +27,50 @@
  * 
  */
 void strassen(unsigned n);
+
+/**
+ * @brief
+ * 
+ * @param A
+ * @param stride_A
+ * @param B
+ * @param stride_B
+ * @param C
+ * @param stride_C
+ * @param n
+ * 
+ */
+void compute_strassen(float *A, unsigned stride_A, float *B, unsigned stride_B,
+                      float *C, unsigned stride_C, unsigned n);
+
+/**
+ * @brief
+ * 
+ * @param X
+ * @param stride_X
+ * @param Y
+ * @param stride_Y
+ * @param Z
+ * @param stride_Z
+ * @param n
+ * 
+ */                     
+void add(float *X, unsigned stride_X, float *Y, unsigned stride_Y,
+         float *Z, unsigned stride_Z, unsigned n);
+
+/**
+ * @brief
+ * 
+ * @param X
+ * @param stride_X
+ * @param Y
+ * @param stride_Y
+ * @param Z
+ * @param stride_Z
+ * @param n
+ * 
+ */                     
+void sub(float *X, unsigned stride_X, float *Y, unsigned stride_Y,
+         float *Z, unsigned stride_Z, unsigned n);
 
 #endif // STRASSEN_H
